7d54dcb6dd5250676de6781c1ff7d8a16fc53f93
Dipzz-newbie <mnadhiftriyudo@gmail.com>
2025-12-11 20:11:01 +0700

    Add: authApi for refreshToken && logout route last add func validation isAuth,getToken, and getRefreshToken

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -1,11 +1,11 @@
 import { apiClient } from "./client"
-import { LoginRequest, RegisterRequest, UserResponse } from "../../models/auth-interfaces"
+import { LoginRequest, RefreshTokenResponse, RegisterRequest, UserResponse } from "../../models/auth-interfaces"
 
 export const authApi = {
 
     async register(data: RegisterRequest): Promise<UserResponse> {
         return apiClient.post<UserResponse>("/api/register", data)
-    }
+    },
 
     async login(data: LoginRequest): Promise<UserResponse> {
         const loginResponse = await apiClient.post<UserResponse>("/api/login", data)
@@ -19,5 +19,52 @@ export const authApi = {
         }
 
         return loginResponse;
+    },
+
+    async refreshToken(): Promise<RefreshTokenResponse> {
+        const refreshToken = localStorage.getItem("refreshToken");
+
+        if(!refreshToken) {
+            throw new Error("Refresh Token not available");
+        }
+
+        const refreshTokenResponse = await apiClient.post<RefreshTokenResponse>("/api/refresh-token", {refreshToken});
+
+        if(refreshTokenResponse.token) {
+            localStorage.setItem("accessToken", refreshTokenResponse.token);
+        }
+
+        if(refreshTokenResponse.refreshToken) {
+            localStorage.setItem("refreshToken", refreshTokenResponse.refreshToken);
+        }
+
+        return refreshTokenResponse;
+    },
+
+    async logout(): Promise<void> {
+        const getRefreshToken = localStorage.getItem("refreshToken");
+
+        if(getRefreshToken) {
+            try{
+                apiClient.post("/api/users/logout", {getRefreshToken});
+            }catch(err) {
+                console.log("Logout Error", err);
+            }
+        }
+
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("refreshToken");
+    },
+
+    isAuthenticated(): boolean {
+        return !!localStorage.getItem("accessToken");
+    },
+
+    getAccessToken(): string | null {
+        return localStorage.getItem("accessToken");
+    },
+
+    getRefreshToken(): string | null {
+        return localStorage.getItem("accessToken")
     }
 }
