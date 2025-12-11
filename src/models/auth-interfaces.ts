6a7d9fc3d149fa497dca929efa6913f45b5d235a
Dipzz-newbie <mnadhiftriyudo@gmail.com>
2025-12-11 19:37:23 +0700

    Add: models authInterfaces for api auth

---
+++ src/models/auth-interfaces.ts
@@ -0,0 +1,26 @@
+export interface RegisterRequest {
+    email: string;
+    password: string;
+    name: string;
+}
+
+export interface LoginRequest {
+    email: string;
+    password: string;
+}
+
+export interface UserResponse {
+    id: string;
+    name: string;
+    email: string;
+    avatarUrl: string | null;
+    createdAt: string;
+    updateAt: string;
+    token?: string;
+    refreshToken?: string;
+}
+
+export interface RefreshTokenResponse {
+    token: string;
+    refreshToken: string;
+}
